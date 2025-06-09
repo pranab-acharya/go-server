114075f107084e1668050fc8a43e58ed784414d6
pranabacharya <pacharya808@gmail.com>
2025-06-09 21:26:33 +0530

    static server working fine

--- main.go
+++ main.go
@@ -2,9 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	var fileServer = http.FileServer(http.Dir("./static"))
 
-	fmt.Println("Hello world!!")
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server on 8000 PORT")
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseFrom() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+
+	fmt.Fprintf(w, "\nName: %s\n", name)
+	fmt.Fprintf(w, "Email: %s\n", email)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!!")
 }
